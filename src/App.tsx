d95381d39ad255655e71c1bbbd5070458e3119bd
SumeetR <sumeetsrai@gmail.com>
2020-06-09 01:26:01 +0200

    remove unused variable

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,6 @@ import NavBar from './NavBar';
 import ImageGrid from './ImageGrid';
 
 function App() {
-  const image = {
-    id: '1',
-    image_url: 'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/01_ValleDelCocora_Colombia_Latinoamerica.jpeg',
-    tags: [
-      'Valle Del Cocora',
-      'Colombia',
-      'Latinoamerica'
-    ]
-  };  
   return (
     <div>
       <NavBar />
