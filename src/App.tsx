13f3dcd5fb88c91bc1167a0eb66e36d30cbdadcb
SumeetR <sumeetsrai@gmail.com>
2020-06-29 23:56:36 +0200

    add tags

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,32 @@ import './App.css';
 import NavBar from './NavBar';
 import ImageGrid from './ImageGrid';
 
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <ImageGrid />
-    </div>
-  );
+
+interface State {
+  tag?: string;
+}
+
+class App extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      tag: undefined
+    };
+  }
+
+  setTag = (tag?: string) => {
+    this.setState({tag});
+  };
+  
+  render() {
+    const {tag} = this.state;
+    return (
+      <div>
+        <NavBar tag={tag} setTag={this.setTag}/>
+        <ImageGrid tag={tag} setTag={this.setTag}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/constants.ts
+++ src/constants.ts
@@ -1,26 +1,10 @@
 export const images = [
-  {
-    img: 'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/08_BocaChica_RD_Latinoamerica.jpeg'
-  },
-  {
-    img: 'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/07_Canarias_Spain_Europe.jpeg'
-  },
-  {
-    img: 'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/06_SenhorDaPedra_Portugal_Europe.jpeg'
-  },
-  {
-    img: 'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/05_AbuDhabi_UAE_MiddleEast.jpeg'
-  },
-  {
-    img: 'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/04_Tulum_Mexico_Latinoamerica.jpeg'
-  },
-  {
-    img: 'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/03_MachuPicchu_Peru_Latinoamerica.jpeg'
-  },
-  {
-    img: `https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/02_Bar'am_Palestine_MiddleEast.jpeg`
-  },
-  {
-    img: 'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/01_ValleDelCocora_Colombia_Latinoamerica.jpeg'
-  }
+  'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/08_BocaChica_RD_Latinoamerica.jpeg',
+  'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/07_Canarias_Spain_Europe.jpeg',
+  'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/06_SenhorDaPedra_Portugal_Europe.jpeg',
+  'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/05_AbuDhabi_UAE_MiddleEast.jpeg',
+  'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/04_Tulum_Mexico_Latinoamerica.jpeg',
+  'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/03_MachuPicchu_Peru_Latinoamerica.jpeg',
+  `https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/02_Bar'am_Palestine_MiddleEast.jpeg`,
+  'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/01_ValleDelCocora_Colombia_Latinoamerica.jpeg'
 ];
