9602d7e50adc585122903369c6d05203f06f6816
Sumcho <sumcho@BatMac-M1.local>
2022-03-22 18:53:36 -0500

    add new image

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -1,4 +1,5 @@
 export const images = [
+  'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/10_Comuna13_Colombia_Latinoamerica.jpeg',
   'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/09_ElMedano_Canarias_Europe.jpeg',
   'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/08_BocaChica_RD_Latinoamerica.jpeg',
   'https://sumcho-dot-com-images.s3-eu-west-1.amazonaws.com/images/07_Tenerife_Canarias_Europe.jpeg',
